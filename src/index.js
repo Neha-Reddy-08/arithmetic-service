f96780190d4795c1c04f52b4a3d471044500fafb
Neha-Reddy-08 <nehareddyvontari8@gmail.com>
2024-04-18 15:12:20 -0400

    p15

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT;
 
  // Serve static files from the 'public' directory
  //
+ 
 app.use(cors());
 app.get('/', (req, res) => {
     res.send('Arithmetic service - Hello world!');
